0365e8442a75be460228951ea1d798bad7abf123
Renan.T <rtoniolo4@gmail.com>
2024-05-23 17:45:07 -0300

    feat: :sparkles: add icons and form mail structure

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,17 @@
 export default function Page() {
-  return <div>Contato</div>;
+  return (
+    <div>
+      <h1>Titulo...</h1>
+      <form action={"qq coisa"}>
+        <input name="name" required />
+        <input type="email" name="email" required />
+        <textarea
+          name="
+    message"
+          required
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 }
